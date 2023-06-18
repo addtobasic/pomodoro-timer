abb3ff272b2063efd23c42d8a5d0456d69f971e9
addtobasic <addtobasic@gmail.com>
2023-06-18 20:13:09 +0900

    Initial Commit

---
+++ pomodoro-timer/pomodoro_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoro_timerApp.swift
+//  pomodoro-timer
+//
+//  Created by genshi on 2023/06/18.
+//
+
+import SwiftUI
+
+@main
+struct pomodoro_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
