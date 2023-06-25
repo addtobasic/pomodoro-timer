8fd26522a6640b43ea4dde03b1edecf60a001e57
addtobasic <addtobasic@gmail.com>
2023-06-25 21:25:17 +0900

    fix: timerの切り替えをPickerからTabViewに修正

--- pomodoro-timer/ContentView.swift
+++ pomodoro-timer/ContentView.swift
@@ -8,24 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedDuration: Int = 25
-
     var body: some View {
         VStack {
-            Picker("Timer Duration", selection: $selectedDuration) {
-                Text("25 Minutes").tag(25)
-                Text("5 Minutes").tag(5)
+            TabView {
+                TimerView(duration: 25 * 60)
+                    .tabItem {
+                        Image(systemName: "desktopcomputer")
+                        Text("25 minutes")
+                    }
+                TimerView(duration: 5 * 60)
+                    .tabItem {
+                        Image(systemName: "cup.and.saucer.fill")
+                        Text("5 minutes")
+                    }
             }
-            .pickerStyle(SegmentedPickerStyle())
-            .padding()
-
-            Spacer()
-
-            // インスタンス更新のために.idを追加
-            TimerView(duration: selectedDuration * 60)
-                .id(selectedDuration)
-            
-            Spacer()
+            .accentColor(Theme.mainColor)
         }
         .background(Theme.backgroundColor)
     }
